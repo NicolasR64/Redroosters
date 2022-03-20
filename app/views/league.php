999403f92b774fdfbe54bd5f56ae5e3506f1cbed
Ravaux Nicolas <la208319@student.helha.be>
2022-03-20 18:18:29 +0100

    UPDATE includes/headerphp {ajout du comportement sticky} + UPDATE views/league.php {rajout des tableaux}

--- app/views/league.php
+++ app/views/league.php
@@ -12,13 +12,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body  class="bg-dark text-light">
-    <header>
-
+    <header class="sticky-top">
+    <?php
+        include('includes/header.php');
+    ?>
     </header>
     <main>
         <!-- League Header -->
         <div class="mt-4 text-center container-fluid profile-header">
-            <h3 class="mt-2 mb-1">U19<!-- <?php echo $leagued->getCategory();?> --></h3>
+            <h3 class="mt-2 mb-1">U19<!-- <?php //echo $leagued->getCategory();?> --></h3>
         </div>
         <!-- League Main -->
         <div class="container rounded mt-2 mb-5">
@@ -26,34 +28,84 @@
                 <div class="col-md-8 border-right mx-auto">
                     <div class="p-3 py-5">
                         <div class="d-flex justify-content-between align-items-center mb-3">
-                            <h4 class="text-right">Calendrier</h4>
+                            <h4 class="text-right">Prochain match :</h4>
                         </div>  
-                        <table class="table-dark w-100">
+                        <table class="table table-dark text-center w-100 table-striped">
                             <thead>
                                 <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Heure</th>
+                                    <th scope="col">Lieu</th>
+                                    <th scope="col">Visiteur</th>
+                                    <th scope="col">Résident</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
+                                    <th scope="row">25-03-22</th>
+                                    <td>19h30</td>
+                                    <td>Charleroi</td>
+                                    <td>Shark</td>
+                                    <td>Redroosters</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
+                                    <th scope="row">31-03-22</th>
+                                    <td>22h30</td>
+                                    <td>Malinne</td>
+                                    <td>Redroosters</td>
+                                    <td>Shark</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
+                                    <th scope="row">14-04-22</th>
+                                    <td>19h30</td>
+                                    <td>Charleroi</td>
+                                    <td>Bulldogs</td>
+                                    <td>Redroosters</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-md-8 border-right mx-auto">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h4 class="text-right">Ancien match :</h4>
+                        </div>  
+                        <table class="table table-dark text-center w-100 table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Heure</th>
+                                    <th scope="col">Lieu</th>
+                                    <th scope="col">Visiteur</th>
+                                    <th scope="col">Résident</th>
+                                    <th scope="col">Score</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">05-03-22</th>
+                                    <td>19h30</td>
+                                    <td>Charleroi</td>
+                                    <td class="text-success">Shark</td>
+                                    <td class="text-danger">Redroosters</td>
+                                    <td><span class="text-success">4</span> - <span class="text-danger">3</span></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">24-02-22</th>
+                                    <td>22h30</td>
+                                    <td>Malinne</td>
+                                    <td class="text-success">Redroosters</td>
+                                    <td class="text-danger">Shark</td>
+                                    <td><span class="text-success">7</span> - <span class="text-danger">2</span></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">15-02-22</th>
+                                    <td>19h30</td>
+                                    <td>Charleroi</td>
+                                    <td class="text-danger">Bulldogs</td>
+                                    <td class="text-success">Redroosters</td>
+                                    <td><span class="text-danger">0</span> - <span class="text-success">9</span></td>
                                 </tr>
                             </tbody>
                         </table>
