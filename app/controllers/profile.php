7a20ceea281d7a6877c8087dfe3c422d4a352566
Ravaux Nicolas <la208319@student.helha.be>
2022-03-21 16:22:34 +0100

    UPDATE FONCTION getUserById() IN controllers/profile.php {optimisation code} + ADD FUNCTION isDataUser() IN controllers/profile.php {permet de savoir si l'utilisateur consulte ses propres données}

--- app/controllers/profile.php
+++ app/controllers/profile.php
@@ -2,19 +2,36 @@
 
 class profile {
 
-    //FAIRE TEST
-    /* récupération d'un joueur par son id */
+    // FAIRE TEST
+    // récupération des données du joueur par son id
     function getUserById(){
         //vérification que l'id est présent
-        if(isset($_GET['id'])){
-            //import du model et création de l'objet model
+        if(isset($_GET['id']) && isset($_SESSION['id'])){
+
+            //importation du model
             require_once('../models/User.php');
             $user = new User();
+
             //récupération de l'utilisateur
             $resultat = $user->getUserByID($_GET['id']);
             // TODO : regarder si $resultat = $user->getUserByID($_GET['id']); marche aussi.
             return $resultat;
         }else{
+            /*
+             * Cela signifie qu'il y a une erreur! 
+             */
+            // TODO : regarder la manière dont on renverra l'erreur!
+        }
+    }
+
+    //vérifie si l'utiliseur demande à voir son propre profil
+    function isDataUser(){
+        if(isset($_GET['id']) && isset($_SESSION['id'])){
+            return 1;
+        }else{
+            /*
+             * Cela signifie qu'il y a une erreur! 
+             */
             // TODO : regarder la manière dont on renverra l'erreur!
         }
     }
