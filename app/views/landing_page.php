27c3f323ada1d4dea3547e3138abb7248c34a626
Mathis Bigras <la200032@student.helha.be>
2022-03-22 10:17:22 +0100

    UPDATE - Chemin d'accès des images.

--- app/views/landing_page.php
+++ app/views/landing_page.php
@@ -18,7 +18,7 @@
 
     <div class="container align-items-center align-middle text-center d-grid mx-auto w-100 vh-100">
       <form class="form-signin" data-bitwarden-watching="1">
-        <img src="/assets/img/logos/logo_icon_text.png" height="100px" loading="lazy"/>
+        <img src="../../assets/img/logos/logo_icon_text.png" height="100px" loading="lazy"/>
         <h1 class="h3 mb-3 font-weight-normal">Redroosters</h1>
         <a class="btn btn-lg btn-primary btn-block mt-1 mb-2 w-100" href="/login">Connexion</a>
         <a class="btn btn-lg btn-secondary btn-block mt-1 mb-2 w-100" href="/register">Inscription</a>
--- app/views/register_a.php
+++ app/views/register_a.php
@@ -18,7 +18,7 @@
 
     <div class="container align-items-center align-middle text-center d-grid mx-auto w-100 vh-100">
       <form class="form-signin" data-bitwarden-watching="1">
-        <img src="/assets/img/logos/logo_icon_text.png" height="100px" loading="lazy"/>
+        <img src="../../assets/img/logos/logo_icon_text.png" height="100px" loading="lazy"/>
         <h1 class="h3 mb-3 font-weight-normal">Redroosters</h1>
         <label for="inputCode" class="sr-only">Code</label>
         <input type="password" id="inputCode" class="form-control" required="" autofocus="">
