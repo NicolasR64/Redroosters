f926656f86446d1b89e178a0f11c7e5d884348ef
Ravaux Nicolas <la208319@student.helha.be>
2022-03-28 10:55:01 +0200

    Changement mineur telle que les noms des fonction ou légère optimisation du code

--- app/views/profile.php
+++ app/views/profile.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+$_SESSION['id']="1";
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -13,33 +17,26 @@
     <?php
         require_once("../controllers/profile.php");
         $contProfile = new Profile();
+        $user = $contProfile->getUserById();
     ?>
 </head>
-<body  class="bg-dark text-light">
+<body>
     <?php
         include('includes/header.php');
     ?>
     <main>
-        <!-- Controller -->
-
-
-
-
-        <?php
-        $user = $contProfile->getUserById();
-        ?>
         <!-- Profile Header -->
         <div class="mt-4 text-center container-fluid profile-header">
             <img class="rounded-circle" src="../../assets/img/avatars/default.jpg" alt="avatars de l'utilisateur" loading="lazy" >
-            <p class="mt-2 mb-1"><?php echo $user['lastName']; echo $user['firstName'];?></p>
+            <p class="mt-2 mb-1"><?php echo $user->getFirstname(); echo $user->getLastName();?></p>
             <?php
-            if(isset($user['function'])){
+            if($user->getIsStaff()){
                 echo '
-                    <p class="text-white-50">'.$user['function'].'</p>
+                    <p class="text-white-50">'.$staff->getFunction().'</p>
                 ';
             }else{
                 echo '
-                    <p class="text-white-50">'.$user['position'].'</p>
+                    <p class="text-white-50">'.$player->getPosition().'</p>
                 ';
             }
             ?>
@@ -53,43 +50,44 @@
                             <h4 class="text-right">Informations générales</h4>
                         </div>      
                         <div class="row mt-2">
-                            <div class="col-md-6 mt-1"><label class="labels mb-2 mt-sm-2">Nom :</label><input type="text" class="form-control" readonly placeholder="Nom" value="<?php echo $user['lastName'] ?>"></div>
-                            <div class="col-md-6 mt-1"><label class="labels mb-2 mt-sm-2">Prénom :</label><input type="text" class="form-control" readonly placeholder="Prenom" value="<?php echo $user['firstName'] ?>"></div>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-sm-2">Surnom :</label><input type="text" class="form-control" readonly placeholder="Surnom" value="<?php echo $user['nickName'] ?>"></div>
+                            <div class="col-md-6 mt-1"><label class="labels mb-2 mt-sm-2">Nom :</label><input type="text" class="form-control" readonly placeholder="Nom" value="<?php echo $user->getLastName() ?>"></div>
+                            <div class="col-md-6 mt-1"><label class="labels mb-2 mt-sm-2">Prénom :</label><input type="text" class="form-control" readonly placeholder="Prenom" value="<?php echo $user->getFirstName() ?>"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-sm-2">Surnom :</label><input type="text" class="form-control" readonly placeholder="Surnom" value="<?php echo $user->getNickname() ?>"></div>
                         </div>
                         <div class="row mt-3">
                         <?php
-                        if(isset($user['function'])){
+                        if($user->getIsStaff()){
                             echo '
-                            <div class="col-md-12 mt-1"><label class="labels mb-2">Position :</label><input type="text" class="form-control" readonly placeholder="Rôle" value="'.$user['function'].'"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2">Position :</label><input type="text" class="form-control" readonly placeholder="Rôle" value="'.$staff->getFunction().'"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Année d\'arrivée :</label><input type="text" class="form-control " readonly placeholder="Année d\'arrivée" value="'.$staff->getSeasonArrived().'"></div>
                             ';
                         }else{
                             echo '
-                            <div class="col-md-12 mt-1"><label class="labels mb-2">Position :</label><input type="text" class="form-control" readonly placeholder="Rôle" value="'.$user['position'].'"></div>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Numéro de maillot :</label><input type="text" class="form-control" readonly placeholder="Numéro de maillot" value="'.$user['jerseyNumber'].'"></div>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Main dominante :</label><input type="text" class="form-control" readonly placeholder="Main dominante" value="'.$user['handedness'].'"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2">Position :</label><input type="text" class="form-control" readonly placeholder="Rôle" value="'.$player->getPostion().'"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Numéro de maillot :</label><input type="text" class="form-control" readonly placeholder="Numéro de maillot" value="'.$player->getJerseyNumber().'"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Main dominante :</label><input type="text" class="form-control" readonly placeholder="Main dominante" value="'.$player->getHandedness().'"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Année d\'arrivée :</label><input type="text" class="form-control " readonly placeholder="Année d\'arrivée" value="'.$player->getSeasonArrived().'"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Numéro de license :</label><input type="text" class="form-control" readonly placeholder="Numéro de license" value="'.$player->getLicenseNumber().'"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Taille (cm) :</label><input type="text" class="form-control  " readonly placeholder="Taille en cm" value="'.$player->getSize().' cm"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Poid (kg) :</label><input type="text" class="form-control " readonly placeholder="Poid en kg" value="'.$player->getWeight().' kg"></div>
                             ';
                         }
                         ?>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Numéro de license :</label><input type="text" class="form-control" readonly placeholder="Numéro de license" value="<?php echo $user['licenseNumber'] ?>"></div>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">E-mail :</label><input type="text" class="form-control" readonly placeholder="E-mail" value="<?php echo $user['mail'] ?>"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">E-mail :</label><input type="text" class="form-control" readonly placeholder="E-mail" value="<?php echo $user->getMail() ?>"></div>
                         <?php
                         if(isset($user['parentMail']))
                             echo'
                                 <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">E-mail d\'un parent :</label><input type="text" class="form-control" readonly placeholder="E-mail parent" value="'.$user['parentMail'].'"></div>
                             ';
                         ?>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Télephone :</label><input type="text" class="form-control"  readonly placeholder="numéro de gsm" value="<?php echo $user['phone'] ?>"></div>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Date de naissance :</label><input type="text" class="form-control" readonly placeholder="Date de naissance" value="<?php echo $user['dateBirth'] ?>"></div>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Année d'arrivée :</label><input type="text" class="form-control " readonly placeholder="Année d'arrivée" value="<?php echo $user['seasonArrived'] ?>"></div>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Taille (cm) :</label><input type="text" class="form-control  " readonly placeholder="Taille en cm" value="<?php echo $user['size'] ?> cm"></div>
-                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Poid (kg) :</label><input type="text" class="form-control " readonly placeholder="Poid en kg" value="<?php echo $user['weight'] ?> kg"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Télephone :</label><input type="text" class="form-control"  readonly placeholder="numéro de gsm" value="<?php echo $user->getPhone() ?>"></div>
+                            <div class="col-md-12 mt-1"><label class="labels mb-2 mt-2">Date de naissance :</label><input type="text" class="form-control" readonly placeholder="Date de naissance" value="<?php echo $user->getDateBirth() ?>"></div>
                         </div>
                         <div class="row mt-3">
                             <div class="d-flex justify-content-end">
                             <?php
-                                if($contProfile->isDataUser()){
-                                    echo'<a class="btn btn-primary" href="profileManagement.php?id='.$player->getId().'">revenir en arrière</a>';
+                                if(isset($_GET['id'])){
+                                    echo'<a class="btn btn-primary" href="profile.php?">Voir son profil</a>';
                                 }
                             ?>
                                 <a class="btn btn-primary" href="homePage.php">revenir en arrière</a>
