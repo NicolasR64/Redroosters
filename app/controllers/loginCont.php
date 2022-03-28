cde11ff89cf7486e18c3837144b03ac62d2c4583
Hugo Noël <la208083@student.helha.be>
2022-03-28 11:10:16 +0200

    ADD FILE loginCont.php - Ajout du controlleur de connection. Il est désormais possible de ce connecter

---
+++ app/controllers/loginCont.php
@@ -0,0 +1,31 @@
+<?php
+    session_set_cookie_params(6000);
+	session_start();
+    require_once("../models/User.php");
+
+    // Elimine les dangers éventuelles pouvant provenir des données entrée par l'utilisateur
+    function cleanData($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+
+    if(isset($_POST["login"]) && !empty($_POST["login"])){
+        if(isset($_POST["inputEmail"]) && !empty($_POST["inputEmail"])){
+            $email = cleanData($_POST["inputEmail"]);
+            if(isset($_POST["inputPassword"]) && !empty($_POST["inputPassword"])){
+                $pwd = cleanData($_POST["inputPassword"]);
+                $user = new User();
+                $user = $user->userConnexion($email,$pwd);
+                if($user != null){
+                    $_SESSION["user"]=serialize($user);
+                    header("Location: http://redrooster/home");
+                }
+            }
+        }
+    }
+
+
+
+?>
