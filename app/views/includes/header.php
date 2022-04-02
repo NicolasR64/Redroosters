5be6567ca66d2cb982780b6b0b6232efab0db483
Mathis Bigras <mathis.bigras@outlook.fr>
2022-04-02 15:35:48 +0200

    ADD header.php - Ajout d'un header temporaire.

--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -1,35 +1,29 @@
 <header class="sticky-top">
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-              <img src="../../assets/img/logos/logo_icon_text.png" alt="logo des redroosters" width="50" height="43" class="d-inline-block align-text-top">
-          </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Équipe</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Statistique de l'équipe</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Évènements</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Messagerie global</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="league.php">League</a>
-            </li>
-            <!-- Seulement si isAdmin = true -->
-            <li class="nav-item">
-              <a class="nav-link" href="#">Page administrateur</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div class="container">
+      <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">
+              <i class="fa-solid fa-house"></i>
+            </a></li>
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="fa-solid fa-user-group"></i>
+            </a>
+          </li>
+          <li class="nav-item"><a href="#" class="nav-link">
+              <i class="fa-solid fa-comments"></i>
+            </a>
+          </li>
+          <li class="nav-item"><a href="#" class="nav-link">
+              <i class="fa-solid fa-chart-column"></i>
+            </a>
+          </li>
+          <li class="nav-item"><a href="#" class="nav-link">
+              <i class="fa-solid fa-user"></i>
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  </nav>
 </header>
