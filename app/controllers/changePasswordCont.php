1b7016e1a588b22babb5f1e385c142387364e414
Hugo Noël <la208083@student.helha.be>
2022-04-19 09:38:54 +0200

    ADD FONCTION changePwd2 - Permet de modifier le mot de passe d'un utilisateur connecté

--- app/controllers/changePasswordCont.php
+++ app/controllers/changePasswordCont.php
@@ -1,6 +1,36 @@
 <?php
 
-$step = 1;
+session_set_cookie_params(6000);
+session_start();
+
+require_once("../models/User.php");
+
+// Elimine les dangers éventuelles pouvant provenir des données entrée par l'utilisateur
+function cleanData($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  $step = 1;
+
+  if(isset($_POST["inputPwd"]) && !empty($_POST["inputPwd"])){
+    if(isset($_POST["inputCPwd"]) && !empty($_POST["inputCPwd"])){
+        $pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/";
+        $pwd  = cleanData($_POST["inputPwd"]);
+        $cPwd = cleanData($_POST["inputCPwd"]);
+        if(preg_match($pattern,$pwd) && strcmp($pwd,$cPwd) == 0){
+                $user = unserialize($_SESSION["user"]);
+                $pwd = password_hash($pwd,PASSWORD_DEFAULT);
+                $user->setPassword($pwd);
+                $user->updatePwd2();
+                $step=2;
+            }
+        }
+    }
+
+
 
 
 ?>
--- app/views/change_password.php
+++ app/views/change_password.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-//require_once("../controllers/chgMdpCont.php");
+require_once("../controllers/changePasswordCont.php");
 ?>
 <html lang="fr">
 
