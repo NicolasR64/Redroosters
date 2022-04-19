b3c2732d7c8c16925204719db9a6f4d60c2ec7da
Lorys <la205408@student.helha.be>
2022-04-19 09:40:34 +0200

    UPDATE event_management.php - require added

--- app/views/event_management.php
+++ app/views/event_management.php
@@ -5,20 +5,12 @@ require_once("../controllers/eventManagementCont.php");
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link href="../../app/css/style.css" rel="stylesheet">
-    <!-- font family -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-
+    <?php require_once("../views/includes/head.php")?>
     <title>Redroosters</title>
 </head>
 <body>
+
+<?php require_once("../views/includes/header.php");?>
 
 <div class="container align-items-center align-middle text-center d-grid mx-auto w-100 vh-100">
       <form method="POST" action="../controllers/eventManagementCont.php" class="form-event" data-bitwarden-watching="1">
@@ -72,5 +64,6 @@ require_once("../controllers/eventManagementCont.php");
         session_destroy();
       }
     ?>
+<?php require_once("../views/includes/footer.php");?>
 </body>
 </html>
