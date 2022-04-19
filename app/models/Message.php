35654e66b0cff5dd95c0b7a94f351e3b499728d9
Coralie_Fremal <coraliefremal23@gmail.com>
2022-04-19 10:13:54 +0200

    ADD FILE Message.php

---
+++ app/models/Message.php
@@ -0,0 +1,31 @@
+<?php
+//connection
+require_once('model.php');
+
+class Message extends model{
+    private $id;
+    private $text;
+    private $date;
+    private $hours;
+    private $idUsers;
+    private $idEvent;   //pas besoin pour le chat global
+
+    public function getMessage($lastId){
+        // On initialise le filtre
+        $filtre = ($lastId > 0) ? " WHERE `message`.`id` > $lastId" : '';
+
+        // On écrit la requêt
+        $sql = 'SELECT `message`.`id`, `message`.`text`, `message`.`date`, `message`.`hours`, `users`.`firstName` FROM `message` LEFT JOIN `users` ON `message`.`idUsers` = `users`.`id`'.$filtre.' ORDER BY `message`.`id` DESC LIMIT 5;';
+
+        // On récupère les données
+        $messages = $this->executeRequest($sql) ;
+
+        // On encode en JSON
+        $messagesJson = json_encode($messages);
+
+        // On envoie
+        return $messagesJson;
+        }
+}
+
+?>
