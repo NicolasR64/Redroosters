fefe24c25708f10365cbb5dd0208559876beafeb
LorysM13 <lorys.massaux@hotmail.com>
2022-04-21 10:24:34 +0200

    UPDATE event_list ADD eventListCont -relation to the database added

--- app/views/event_list.php
+++ app/views/event_list.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <?php require_once("../views/includes/head.php")?>
+    <?php require_once("../views/includes/head.php");
+          require_once("../controllers/eventListCont.php");
+    ?>
     <script src="/app/js/checkButtonStatus.js" async></script>
     <title>Redroosters</title>
 </head>
@@ -11,26 +13,71 @@
 
 <h1 class="h3 mb-3 font-weight-normal text-center">Liste des évènements à venir</h1>
 
-<div class ="event-block align-middle mx-auto">
-    <span class="title">Match de hockey<br></span>
+<?php
+if(!isset($upcomingEvents) || $upcomingEvents == ""){
+    echo "<div class='text-center'>Il n'y a pas d'évènements à afficher</div><br>";
+}else{
+foreach($upcomingEvents as $elem){
+    echo "<div class ='event-block align-middle mx-auto'>";
+    echo "<span class='title'>".$elem->getName()."<br></span>";
+?>
     <div class ="text-flex-row">
         <div class="text-flex-column-first">
-             <br>
                 <div class="informations">
-                 <div class="dateHeure">13/08</div>
+                <?php
+                echo "<div class='dateHeure'>".$elem->getDateBegin()."</div>";
+                ?>
                 <div class="heure">10H</div>
                 <div class="minutes">08</div>
             </div>
         </div>   
         <div class="text-flex-column"> 
-            <div class="adresse">Rue de Linux, 6666 TuxCity</div>
-             <div class="timeLeft">Dans 7 jours</div><br>
-             <button class="btn btn-danger" id="btn">Absent</button><br>
+            <?php
+            echo "<div class='adresse'>".$elem->getStreet().", ".$elem->getPostalCode()." ".$elem->getCity()."</div>";
+            echo "<div class='timeLeft'>Dans ".calculateDays($elem->getDateBegin())."</div><br>";
+            ?>
         </div>
     </div>
 </div>
-<br><br>
+<?php
+} 
+}
+?>
+<br>
 <h1 class="h3 mb-3 font-weight-normal text-center">Liste des évènements passés</h1>
+
+<?php
+if(!isset($pastEvents) || $pastEvents == ""){
+    echo "<div class='text-center'>Il n'y a pas d'évènements à afficher</div><br>";
+}else{
+
+foreach($pastEvents as $elem){
+    echo "<div class ='event-block align-middle mx-auto'>";
+    echo "<span class='title'>".$elem->getName()."<br></span>";
+?>
+    <div class ="text-flex-row">
+        <div class="text-flex-column-first">
+             <br>
+                <div class="informations">
+                <?php
+                echo "<div class='dateHeure'>".$elem->getDateBegin()."</div>";
+                ?>
+                <div class="heure">10H</div>
+                <div class="minutes">08</div>
+            </div>
+        </div>   
+        <div class="text-flex-column"> 
+            <?php
+            echo "<div class='adresse'>".$elem->getStreet().", ".$elem->getPostalCode()." ".$elem->getCity()."</div>";
+            ?>
+        </div>
+    </div>
+</div>
+<br>
+<?php
+} 
+}
+?>
 </div>
 <?php require_once("../views/includes/footer.php");?>
 </body>
