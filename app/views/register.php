37e5dfd75e0fe23038f680797a2879adfcdf50b7
Mathis Bigras <mathis.bigras@outlook.fr>
2022-04-22 10:11:04 +0200

    ADD FILE isDisconnect.php - Redirige l'utilisateur vers la page /home si connecté.

--- app/views/register.php
+++ app/views/register.php
@@ -1,3 +1,11 @@
+<?php
+
+session_start();
+
+require_once("../controllers/isDisconnect.php");
+
+?>
+
 <!DOCTYPE html>
 <?php
 require_once("../controllers/registerCont.php");
@@ -6,22 +14,9 @@ require_once("../controllers/registerCont.php");
 
 <head>
 
-    <!-- REMPLACER ICI -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link href="../../app/css/style.css" rel="stylesheet">
-    <!-- font family -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <!-- JUSQU'ICI -->
+  <?php require_once("../views/includes/head.php"); ?>
 
-    <script src="/app/js/checkInputRegister.js" async></script>
-
-    <title>Inscription</title>
+  <title>Inscription | Redroosters</title>
 
 </head>
 
