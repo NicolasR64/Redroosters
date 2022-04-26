f17e1ab936cc0e054492539eb75417d248ffa083
Hugo Noël <la208083@student.helha.be>
2022-04-26 08:51:53 +0200

    ADD FONCTION getAllUsers() - permet de récupérer tout les utilisateurs de la db

---
+++ app/views/invitation.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+    require_once("../controllers/invitationCont.php");
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <?php require_once("../views/includes/head.php")?>
+        <title>Redroosters</title>
+    </head>
+    <body>
+        <?php require_once("../views/includes/header.php");?>
+    </body>
+</html>
