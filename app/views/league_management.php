cb339d7658f338b8dea623e312cc267538cb1cbb
CoralieFr <101631136+CoralieFr@users.noreply.github.com>
2022-04-30 19:56:53 +0200

    UPDATE FILE "league_management.php"
    modif des champs + inclusion du js

--- app/views/league_management.php
+++ app/views/league_management.php
@@ -1,7 +1,15 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
+<?php
+    require_once("../controllers/leagueManagementCont.php");  
+?>
 <html lang="fr">
 <head>
     <?php require_once("../views/includes/head.php")?>
+    <script src="/app/js/checkInputLeagueManagement.js" async></script>  <!--pour faire les vérifs --> 
     <title>Redroosters</title>
 </head>
 <body>
@@ -9,26 +17,21 @@
 <?php require_once("../views/includes/header.php");?>
 
 <div class="container align-items-center align-middle text-center d-grid mx-auto w-100 vh-100">
-      <form method="POST" action="../controllers/eventManagementCont.php" class="form-event" data-bitwarden-watching="1" id="formLeagueManagement">
-      <img src="../../assets/img/logos/logo_icon_text.png" height="100px" loading="lazy"/>
+      <form method="POST" action="../controllers/leagueManagementCont.php" class="form-league" data-bitwarden-watching="1" id="formLeagueManagement">
+      <img src="/assets/img/logos/logo_icon_text.png" height="100px" loading="lazy"/>
       <h1 class="h3 mb-3 font-weight-normal">Ajouter/Modifier une league</h1>
 
         <label for="inputCategorie">Catégorie</label>
-        <input type="text" name="inputCategorie" id="inputCategorie" class="form-control" required="" autofocus="">
+        <input type="text" name="inputCategorie" id="inputCategorie" class="form-control"  autofocus="">
 
         <label for="inputSeasonYear">Année de la saison</label>
-        <input type="number" name="inputSeasonYear" id="inputSeasonYear" class ="form-control" required="" autofocus="">
+        <input type="number" name="inputSeasonYear" id="inputSeasonYear" class ="form-control" >
 
-        <button class="btn btn-lg btn-primary btn-block mt-1 mb-2 w-100" id="submitButton" type="submit" name="form-event">Ajouter league</button>
+        <button class="btn btn-lg btn-primary btn-block mt-1 mb-2 w-100" id="submitButton" type="submit" name="form-league">Ajouter league</button>
         <p class="mt-5 mb-3 text-muted">© Projet HELHa</p>
       </form>
     </div>
-    <?php
-      if(isset($_SESSION["error"])){
-        echo $_SESSION["error"];
-        session_destroy();
-      }
-    ?>
+    
 <?php require_once("../views/includes/footer.php");?>
 </body>
 </html>
