7a60cae933df7c308a3a178fa9e48c0e623ffc9b
Mat <mathis.bigras@outlook.fr>
2022-05-09 20:34:24 +0200

    UPDATE FILE memberlist.php - Correction du design.

--- app/views/memberlist.php
+++ app/views/memberlist.php
@@ -30,45 +30,29 @@ require_once("../controllers/memberListCont.php");
 
   <h1>Liste des membres</h1>
   <div class="container align-items-center align-middle text-center d-grid mx-auto w-100 vh-100">
-    <table>
-      <thead>
-        <tr>
-          <th>Joueur</th>
-          <th>Position</th>
-          <th>Fonction</th>
-          <?php if (unserialize($_SESSION["user"])->getIsAdmin() == 1) { ?>
-            <th>Outil</th>
-          <?php } ?>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        if (!empty($joueurs)) {
-          foreach ($joueurs as $row) {
-        ?>
-            <tr>
-              <?php if ($row->getId() == unserialize($_SESSION["user"])->getId()) { ?>
-                <td><a href="/profile"><?php echo $row->getFirstName() . " " . $row->getLastName(); ?></a></td>
-              <?php } else { ?>
-                <td><a href="/profile/<?php echo $row->getId(); ?>"><?php echo $row->getFirstName() . " " . $row->getLastName(); ?></a></td>
-              <?php } ?>
-              <td><?php echo $row->getPlayer(); ?></td>
-              <td><?php echo $row->getStaff(); ?></td>
-              <?php if (unserialize($_SESSION["user"])->getIsAdmin() == 1) { ?>
-                <td>
-                  <a href='<?php echo $url; ?>?changePlayerState=<?php echo $row->getId(); ?>'><?php if($row->getIsPlayer() == 0){ ?>Désigner joueur<?php } else { ?>Destituer Joueur<?php }?></a>
-                  <a href='<?php echo $url; ?>?changeAdminState=<?php echo $row->getId(); ?>'><?php if($row->getIsAdmin() == 0){ ?>Désigner administrateur<?php } else { ?>Destituer administrateur<?php }?></a>
-                  <a href='<?php echo $url; ?>?changeStaffState=<?php echo $row->getId(); ?>'><?php if($row->getIsStaff() == 0){ ?>Désigner membre du personnel<?php } else { ?>Destituer membre du personnel<?php }?></a>
-                  <a href='<?php echo $url; ?>?delete=<?php echo $row->getId(); ?>'>Supprimer</a>
-                </td>
-              <?php } ?>
-            </tr>
-        <?php
-          }
+
+    <div class="event-brick-grid">
+
+      <?php
+      if (!empty($joueurs)) {
+        foreach ($joueurs as $row) {
+      ?>
+
+          <a href="/profile/<?php echo $row->getId(); ?>" class="event-brick-cell">
+            <h2><?php echo $row->getFirstName() ?></h2>
+            <span class="date"><?php echo $row->getPlayer(); ?></span>
+            <span class="time-left">
+              <?php echo $row->getStaff(); ?>
+            </span>
+          </a>
+
+      <?php
         }
-        ?>
-      </tbody>
-    </table>
+      }
+      ?>
+
+    </div>
+
   </div>
 
   <?php require_once("../views/includes/footer.php"); ?>
