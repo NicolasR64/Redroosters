4630a05c16ec85990fcecea6897e35c1b54db026
Mat <mathis.bigras@outlook.fr>
2022-05-10 09:54:35 +0200

    ADD FILE noscript.php - Empêche la désactivation du JavaScript.

---
+++ app/views/includes/noscript.php
@@ -0,0 +1,5 @@
+<noscript>
+    <div class="noscript">
+        <h1 class="container align-items-center align-middle text-center d-grid mx-auto w-100 vh-100">JavaScript est désactivé</h1>
+    </div>
+</noscript>
