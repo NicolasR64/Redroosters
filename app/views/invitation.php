97a472cb19a7663510dafa8cf45da375db3ab32e
Ravaux Nicolas <la208319@student.helha.be>
2022-05-13 09:27:12 +0200

    UPDATE FILE style.css and invitation.php {changement visuelle}

--- app/views/invitation.php
+++ app/views/invitation.php
@@ -15,46 +15,43 @@
             if($done == 0){
         ?>
             <div class="container align-items-center align-middle text-center d-grid mx-auto w-100 vh-100">
-                <form class="form-signin" data-bitwarden-watching="1" method="post" name="inv" id="inv">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Joueur</th>
-                                <th>Position</th>
-                                <th>Fonction</th>
-                                <th>Invité?</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                                if(!empty($joueurs)) { 
-                                    foreach($joueurs as $row) {
-                            ?>
-                                <tr>
-                                    <td><?php echo $row->getFirstName() . " " . $row->getLastName(); ?></td>
-                                    <td><?php echo $row->getPlayer(); ?></td>
-                                    <td><?php echo $row->getStaff(); ?></td>
-                                    <td><input type="checkbox" name="invite[]" value=<?php echo $row->getId(); ?>></td>
-                                </tr>
-                            <?php
-                                    }
-                                }
-                            ?>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td colspan="4"><input type="checkbox" id="all">Inviter tous les joueurs</td>
-                            </tr>    
-                        </tfoot>
-                    </table>
-                    <button class="btn btn-lg btn-primary btn-block mt-1 mb-2 w-100" type="submit" id="invButton"name="invit" value="inv">Inviter</button>
+                <form class="form-listPlayer" data-bitwarden-watching="1" method="post" name="inv" id="inv">
+                    <div class="d-flex d-md-row listPlayerContainer">
+                    <?php
+                    if(!empty($joueurs)) { 
+                        foreach($joueurs as $row) {
+                    ?>
+                        <div class="border-end border-bottom border-top border-2 rounded listPlayer">
+                            <div class="border-start border-danger border-5 rounded">
+                                <div>
+                                    <?php echo $row->getFirstName() . " " . $row->getLastName(); ?>
+                                </div>
+                                <div>
+                                    <?php 
+                                        echo 'Position : '.$row->getPlayer();
+                                        echo'<br>';
+                                        echo 'Fonction : '.$row->getStaff(); 
+                                    ?>
+                                </div>
+                                <input type="checkbox" name="invite[]" value=<?php echo $row->getId(); ?>>
+                            </div>
+                        </div>
+                    <?php
+                        }
+                    }
+                    ?>
+                    </div>
+                    <input type="checkbox" id="all">Inviter tous les joueurs
+                    <br>
+                    <button class="btn btn-lg btn-primary btn-block mt-1 mb-2 w-50 w-md-25" type="submit" id="invButton"name="invit" value="inv">Inviter</button>
                 </form>
             </div>
         <?php } 
             if ($done == 1){
         ?>
-            <h2>Invitations envoyées&nbsp;!</h2>
-                <a class="btn btn-lg btn-primary btn-block mt-1 mb-2 w-100" href="/home">Accueil</a>
+            <h2 class="w-25">Invitations envoyées&nbsp;!</h2>
+                <a class="btn btn-lg btn-primary btn-block mt-1 mb-2 w-25" href="/events">Liste évènement</a>
         <?php } ?>
+        <?php require_once("../views/includes/footer.php"); ?>
     </body>
 </html>
