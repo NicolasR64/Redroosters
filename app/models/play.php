771a5f84093945cc8ef698d3ea16e302d34979af
Hugo Noël <la208083@student.helha.be>
2022-05-15 11:39:21 +0200

    ADD FILE play.php - ajout du modèle de la table play (qui permet de faire le lien entre les équipes et les matchs)

---
+++ app/models/play.php
@@ -0,0 +1,63 @@
+<?php
+require_once('Model.php');
+
+class Play extends Model{
+    private $idTeam;
+    private $idMatch;
+    private $notation;
+
+
+    /**
+     * Get the value of idTeam
+     */ 
+    public function getIdTeam()
+    {
+        return $this->idTeam;
+    }
+
+    /**
+     * Set the value of idTeam
+     *
+     */ 
+    public function setIdTeam($idTeam)
+    {
+        $this->idTeam = $idTeam;
+    }
+
+    /**
+     * Get the value of idMatch
+     */ 
+    public function getIdMatch()
+    {
+        return $this->idMatch;
+    }
+
+    /**
+     * Set the value of idMatch
+     *
+     */ 
+    public function setIdMatch($idMatch)
+    {
+        $this->idMatch = $idMatch;
+
+    }
+
+    /**
+     * Get the value of notation
+     */ 
+    public function getNotation()
+    {
+        return $this->notation;
+    }
+
+    /**
+     * Set the value of notation
+     *
+     */ 
+    public function setNotation($notation)
+    {
+        $this->notation = $notation;
+    }
+}
+
+?>
