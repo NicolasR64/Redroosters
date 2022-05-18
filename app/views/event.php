106807f88c0d1928161361f79132d908a5abd1ee
Ravaux Nicolas <la208319@student.helha.be>
2022-05-18 10:54:22 +0200

    UPDATE FILE event.php {rajout boutton d'envoi de rappel}

--- app/views/event.php
+++ app/views/event.php
@@ -223,6 +223,11 @@
                         </div>
                   </div>
             </div>
+            <div class="row">
+                  <div class="col-md-12 text-center">
+                        <a class="btn btn-primary" href="/app/controllers/reminderCont.php">Renvoyer un rappel</a>
+                  </div>
+            </div>
       </div>
       <?php
       if ($sessionUser->getIsAdmin() == 1) {
